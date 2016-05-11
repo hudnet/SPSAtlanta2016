3b8fcc772bed8561428d9b44c1f26b5615ef54c8
Eric Hudson <eric@hudnet.com>
2016-05-11 18:06:42 -0500

    Create usage.js

---
+++ usage.js
@@ -0,0 +1,60 @@
+$(function() {
+    // Radialize the colors
+    Highcharts.getOptions().colors = Highcharts.map(Highcharts.getOptions()
+        .colors, function(color) {
+            return {
+                radialGradient: {
+                    cx: 0.5,
+                    cy: 0.3,
+                    r: 0.7
+                },
+                stops: [
+                    [0, color],
+                    [1, Highcharts.Color(color).brighten(-0.3).get(
+                        'rgb')] // darken
+                ]
+            };
+        });
+    // Build the chart
+    $('#container').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
+        },
+        title: {
+            text: 'Uasge by Department'
+        },
+        tooltip: {
+            pointFormat: '<b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme
+                            .contrastTextColor) || 'black'
+                    },
+                    connectorColor: 'silver'
+                }
+            }
+        },
+        series: [{
+            data: [{
+                name: 'HR',
+                y: 40
+            }, {
+                name: 'IT',
+                y: 21
+            }, {
+                name: 'Sales',
+                y: 31
+            }]
+        }]
+    });
+});
